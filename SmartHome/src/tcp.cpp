22f465dde71058b9294102c18d8d70803adbeb47
Cyruswen <1321336357@qq.com>
2018-08-08 23:04:12 +0800

    add tcp.cpp

--- SmartHome/src/tcp.cpp
+++ SmartHome/src/tcp.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <iostream>
 #include <ctype.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,80 +9,128 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 #include <sys/wait.h>
+#include <vector>
+
 
 #define MAXSIZE 1024
+
+typedef struct sockid{
+    int id;
+    int client_fd;
+} sockid;
 
 typedef struct Arg{
     int fd;
     struct sockaddr_in addr;
+    std::vector<sockid> Mq;
 } Arg;
+
 
 void Usage()
 {
     printf("Usage:./server port\n");
 }
 
-void getline(int client_fd)
+//http发来的报文：app id close/open
+//mcu发来的报文： mcu id connect
+
+int getline(int client_fd, char* buf, char* source, char* id,  char* directive)
 {
-    char source[MAXSIZE/4] = {0};
-    char directive[MAXSIZE/4] = {0};
-    char buf[MAXSIZE] = {0};
     ssize_t read_size = read(client_fd, buf, sizeof buf);
     if(read_size == 0)
     {
         close(client_fd);
-        return;
+        return -1;
     }
     size_t i = 0;
     size_t j = 0;
-    while(!isspace(buf[i]) && i < sizeof(buf) && i < sizeof(source))
+    while(!isspace(buf[i]))
     {
         source[j++] = buf[i++];
     }
     source[j] = '\0';
-    printf("%s\n", source);
+    //printf("%s\n", source);
     //走到这儿，将客户端确定
-    while(isspace(buf[i]) && i < sizeof(buf))
+    while(isspace(buf[i]))
+    {
+        i++;
+    }
+    j = 0;
+    while(!isspace(buf[i]))
+    {
+        directive[j++] = buf[i++];
+    }
+    directive[j] = '\0';
+    //走到这儿读到命令
+    while(isspace(buf[i]))
     {
         i++;
     }
     j = 0;
     while(buf[i] != '\0')
     {
-        directive[j++] = buf[i++];
+        id[j++] = buf[i++];
     }
-    directive[j] = '\0';
-    printf("%s\n", directive);
+    id[j] = '\0';
+    //读到id
+    return 1;
 }
 
-void ProcessRequest(int client_fd, struct sockaddr_in* client_addr){
-    char buf[1024] = {0};
-    for(;;){
-        //TODO
-        ssize_t read_size = read(client_fd, buf, sizeof(buf));
-        if(read_size < 0){
-            perror("read");
-            continue;
-        }
-        //inet_ntoa 将十进制网络序列转换为点分十进制
-        if(read_size == 0){
-            printf("client %s say goodbye!\n", inet_ntoa(client_addr->sin_addr));
-            close(client_fd);
+int findSockId(int id, std::vector<sockid> mq)
+{
+    size_t i = 0;
+    for(; i < mq.size(); i++)
+    {
+        if(mq[i].id == id)
+        {
             break;
         }
-        buf[read_size] = '\0';
-        printf("client: %s say: %s\n", inet_ntoa(client_addr->sin_addr), buf);
-        write(client_fd, buf, strlen(buf));
     }
-    return;
+    if(i >= mq.size())
+    {
+        return -1;
+    }
+    return mq[i].client_fd;
+}
+
+void ProcessRequest(int client_fd, std::vector<sockid> mq){
+    //tcp ok
+    char source[MAXSIZE/4] = {0};
+    char directive[MAXSIZE/4] = {0};
+    char buf[MAXSIZE] = {0};
+    char id[MAXSIZE/4] = {0};
+    getline(client_fd, buf, source, id,  directive);
+    if(strcasecmp(source, "mcu") == 0)
+    {
+        //如果是mcu，
+        //1.需要将它维护起来
+        //2.发送一个响应让其亮一下
+        sockid sd;
+        sd.client_fd = client_fd;
+        sd.id = atoi(id);
+        mq.push_back(sd);
+        char respond_mcu[] = "tcp ok";
+        send(client_fd, respond_mcu, sizeof(respond_mcu), 0);
+    }
+    else if(strcasecmp(source, "app") == 0)
+    {
+        //如果是app,
+        //1.需要响应一个接收成功报文
+        //2.需要根据id找到mcu的fd
+        char respond[] = "tcp ok";
+        send(client_fd, respond, sizeof(respond), 0);
+        int mcu_fd = findSockId(atoi(id), mq);
+        send(mcu_fd, directive, sizeof(directive), 0);
+    }
 }
 
 
 void* CreateWorker(void* ptr )
 {
     Arg* arg = (Arg*)ptr;
-    ProcessRequest(arg->fd, &arg->addr);
+    ProcessRequest(arg->fd, arg->Mq);
     free(arg);
+    return NULL;
 }
 
 int main(int argc,char* argv[])
@@ -91,6 +140,7 @@ int main(int argc,char* argv[])
         Usage();
         return -1;
     }
+    std::vector<sockid> mq;
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(atoi(argv[1]));
@@ -131,9 +181,9 @@ int main(int argc,char* argv[])
         Arg* arg = (Arg*)malloc(sizeof(Arg));
         arg->fd = client_fd;
         arg->addr = client_addr;
+        arg->Mq = mq;
         pthread_create(&tid, NULL, CreateWorker, (void*)arg);
         pthread_detach(tid);
     }
     return 0;
 }
-
